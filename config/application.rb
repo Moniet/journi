d31483b822787a4453a153a8cca494d5c8d84bb7
moniet <moniet.sawhney@gmail.com>
2019-10-06 18:36:24 +0530

    added cors origin

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module JourniBackend
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'https://journi.netlify.com'
         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
