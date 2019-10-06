285464d52ca5da924333e0eb2c826e9d9dd616a2
moniet <moniet.sawhney@gmail.com>
2019-10-06 18:32:09 +0530

    removed token argument from create_posts

--- app/graphql/mutations/create_post.rb
+++ app/graphql/mutations/create_post.rb
@@ -1,15 +1,13 @@
 class Mutations::CreatePost < Mutations::BaseMutation
     argument :title, String, required: true
     argument :body, String, required: true
-    argument :token, String, required: true
   
     field :message, String, null: true
     field :errors, [String], null: false
     field :post, Types::PostType, null: true
   
-    def resolve(title:, body:, token:)
-        decoded_token = JWT.decode(token, ENV['SECRET_KEY_BASE'], true, { algorithm: 'HS256' })
-        user = User.find decoded_token[0]["user_id"]
+    def resolve(title:, body:)
+        user = context[:current_user]
 
         if user
             post = Post.create title: title, body: body, user_id: user.id
